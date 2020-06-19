44d9f23eceff86aff8af947ffe8131df05a2af3d
Alex Baulderstone <alex.baulderstone@gmail.com>
2020-06-19 09:42:44 +0930

    changed selector to controlled component

--- src/App.js
+++ src/App.js
@@ -1,12 +1,18 @@
-import React from 'react'
+import React, {useState} from 'react'
 import CurrencySelector from './Components/CurrencySelector';
 import DataDisplay from './Components/DataDisplay'
 
 const App = () => {
+  const defaultCurrency = 'AUD'
+  
+  const [currency, setCurrency]= useState(defaultCurrency);
+  const handleCurrencyChange = (newCurrency) => {
+    setCurrency(newCurrency);
+  }
   return (
     <div >
           <h1> BitCoin Index</h1>
-          <CurrencySelector /> 
+          <CurrencySelector currency={currency} onCurrencyChange={handleCurrencyChange} /> 
           <DataDisplay />
     </div>
   )
--- src/Components/CurrencySelector.js
+++ src/Components/CurrencySelector.js
@@ -1,6 +1,17 @@
 import React from 'react';
-const CurrencySelector = () => {
-    return null
+import supportedCurrencies from '../supported-currencies.json';
+
+const CurrencySelector = (props) => {
+    const {currency, onCurrencyChange} = props;
+    return (
+        <div> 
+            <select value={currency} onChange={(event) => onCurrencyChange(event.target.value)}>
+            {supportedCurrencies.map ((item, index) => {
+                return <option value={item.currency} key={index}>{item.country}</option>
+            })}
+            </select>
+        </div>
+    )
 }
 
 export default CurrencySelector;
